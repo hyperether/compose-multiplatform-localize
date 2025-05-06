63392676dec82063e2201f600c29a050382fa494
nikolahadzic <nikola.hadzic@hotmail.com>
2025-05-06 16:03:13 +0200

    Get locale by code Get all available locales in app

--- demo-app/composeApp/src/commonMain/kotlin/com/hyperether/localization/App.kt
+++ demo-app/composeApp/src/commonMain/kotlin/com/hyperether/localization/App.kt
@@ -1,10 +1,14 @@
 package com.hyperether.localization
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.hyperether.resources.AppLocale
 import com.hyperether.resources.currentLanguage
 import com.hyperether.resources.stringResource
@@ -38,6 +43,30 @@ fun App() {
                     if (currentLanguage.value == AppLocale.DEFAULT) AppLocale.DE else AppLocale.DEFAULT
             }) {
                 Text("Click me!")
+            }
+
+            Text("All supported locales: ")
+            AppLocale.supportedLocales.forEach {
+                Text("${it.key}, ${it.value}")
+            }
+
+            Spacer(modifier  = Modifier.height(20.dp))
+
+            Text("Find by code: ")
+            Text("${AppLocale.findByCode("de")}")
+            Text("${AppLocale.findByCode("ll")}")
+
+            Spacer(modifier  = Modifier.height(20.dp))
+
+            Text("Set locale with code: ")
+            var code by remember { mutableStateOf("") }
+            Row {
+                TextField(code, { code = it })
+                Button(onClick = {
+                    currentLanguage.value = AppLocale.findByCode(code)
+                }) {
+                    Text("Change")
+                }
             }
 
         }
